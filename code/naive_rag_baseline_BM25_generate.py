382788f08d347d17f1db18a0e0208e6038d3b3dd
Yixiao-Song <yixiao.song.syx@gmail.com>
2024-11-23 18:40:43 +0000

    Naive retrieval with BM25

---
+++ code/naive_rag_baseline_BM25_generate.py
@@ -0,0 +1,68 @@
+"""
+This script is to get the oracle (upper bound) for the model performance.
+"""
+
+import os
+import ast
+import pdb
+import json
+import utils
+import argparse
+from tqdm import tqdm
+from GetResponse import GetResponse
+
+# add baseline_name to the parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--baseline_type", type=str, default="oracle")
+args = parser.parse_args()
+
+if args.baseline_type == "oracle":
+    key_to_ans = "Qwen_oracle_answer"
+elif args.baseline_type == "zero_shot":
+    key_to_ans = "Qwen_zero_shot_answer"
+
+"""
+LOAD DATA
+"""
+frames_file = "data/Qwen_Outputs/naive_rag_baseline_BM25_retrieve.jsonl"
+with open(frames_file, "r") as f:
+    frames_data = [json.loads(x.strip()) for x in f.readlines() if x.strip()]
+print("Loaded frames_data.")
+
+wiki_url_contents_file = "data/wikipedia/jsonl_output/wikipedia_filtered_url_to_content.json"
+with open(wiki_url_contents_file, "r") as f:
+    wiki_url_contents_dict = json.load(f)
+print("Loaded wiki_url_contents_dict.")
+
+"""
+GENERATE RESPONSE
+"""
+qwen_model = GetResponse()
+print("Qwen model loaded.")
+
+naive_BM25_output_file = "data/Qwen_Outputs/naive_BM25_output.jsonl"
+start_point = utils.get_start_point(naive_BM25_output_file)
+
+with open(naive_BM25_output_file, "a") as f:
+    for dict_item in tqdm(frames_data[start_point:]):
+        context = utils.prepare_context(
+            dict_item,
+            wiki_url_contents_dict,
+            key_to_links='naive_rag_retrieve_results'
+            )
+        query = dict_item["Prompt"]
+
+        prompt = utils.oracle_prompt_template.format(
+            context=context,
+            question=query
+            )
+
+        response = qwen_model.get_response(prompt)
+        grouth_truth_ans = dict_item["Answer"]
+
+        print(f"Ground truth answer: {grouth_truth_ans}")
+        print(f"Qwen response: {response}")
+
+        dict_item["Qwen_naive_BM25_answer"] = response.strip()
+
+        f.write(json.dumps(dict_item) + "\n")
---
+++ code/naive_rag_baseline_BM25_retrieve.py
@@ -0,0 +1,43 @@
+import os
+import ast
+import pdb
+import json
+import utils
+from tqdm import tqdm
+from GetResponse import GetResponse
+from pyserini.search.lucene import LuceneSearcher
+
+"""
+LOAD DATA
+"""
+frames_file = "data/frames_dataset_2_5_links_filtered.jsonl"
+with open(frames_file, "r") as f:
+    frames_data = [json.loads(x.strip()) for x in f.readlines() if x.strip()]
+print("Loaded frames_data.")
+
+"""
+SET UP BM25 Retriever
+"""
+searcher = LuceneSearcher('data/wikipedia/index_output_filtered_wiki')
+def BM25_searcher(query):
+    hits = searcher.search(query, k=5)
+    return hits
+
+out_file = "data/naive_rag_baseline_BM25_retrieve.jsonl"
+with open(out_file, "w") as f:
+    for dict_item in tqdm(frames_data):
+        query = dict_item["Prompt"]
+        print(f"query: {query}")
+        hits = BM25_searcher(query)
+
+        url_score_tuple_lst = []
+        for i in range(len(hits)):
+            docid = hits[i].docid
+            score = hits[i].score
+
+            raw_doc_dict = ast.literal_eval(searcher.doc(docid).raw())
+
+            url_score_tuple_lst.append((raw_doc_dict['url'], score))
+
+        dict_item["naive_rag_retrieve_results_BM25"] = url_score_tuple_lst
+        f.write(json.dumps(dict_item) + "\n")
